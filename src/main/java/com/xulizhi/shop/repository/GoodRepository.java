a90fe40ea158d8496fe5de77e2fe36bf4e1fdac4
SanDaDongLiRoad <1013310985@qq.com>
2018-12-03 23:43:47 +0800

    修改保存类目相关方法

--- src/main/java/com/xulizhi/shop/repository/GoodRepository.java
+++ src/main/java/com/xulizhi/shop/repository/GoodRepository.java
@@ -3,8 +3,12 @@ package com.xulizhi.shop.repository;
 import com.xulizhi.shop.domain.Good;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author lenovo
  */
 public interface GoodRepository extends JpaRepository<Good,String>{
+
+    List<Good> findByStatus(Integer status);
 }
